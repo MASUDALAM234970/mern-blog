f09ec04141c0af1367b100f52fb5d8e3741e23ba
mdmas <mdmasudalam2349@gmail.com>
2024-12-05 09:47:54 +0600

    Connect to the Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose"; // Correct the typo here
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongoose is a connected");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  }); // Add error handling for the database connection
+
 const app = express();
 
 app.listen(3000, () => {
