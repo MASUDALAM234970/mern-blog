74d68fe2665d83aca3dbd9c8a29d162e6001fbbe
mdmas <mdmasudalam2349@gmail.com>
2024-12-10 18:56:48 +0600

    Create a SiGnup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="">{/* left */}</div>
+      <div>
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+          tes
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Alam's
+          </span>
+          Blog
+        </Link>
+      </div>
+      <div className="">{/* Right*/}</div>
+    </div>
+  );
 };
