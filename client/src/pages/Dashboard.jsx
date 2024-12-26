523e3ed854389cb5edb657ebb60106169397bea4
mdmas <mdmasudalam2349@gmail.com>
2024-12-27 00:55:01 +0600

    Create DashSidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import DashSidebar from "../Component/DashSidebar";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTabs] = useState("");
+  useEffect(() => {
+    const urlparams = new URLSearchParams(location.search);
+    const tabFromUrl = urlparams.get("tab");
+    if (tabFromUrl) {
+      setTabs(tabFromUrl);
+      console.log(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <div>
+      <div className="">
+        {/* sidebar*/}
+        <DashSidebar />
+      </div>
+      <div className="">
+        {/* profile..... */}
+        {tab == "profile" && <div>Profile</div>}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
