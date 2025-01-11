ed0e93834f362ba30a7d498122a81a9342d3c288
mdmas <mdmasudalam2349@gmail.com>
2025-01-11 14:42:14 +0600

    add edit functionality comment componemt

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { verifyTokens } from "../utils/verifyUser.js";
 import {
   createComment,
+  editComment,
   getPostComments,
   likeComment,
 } from "../controllers/comment.controller.js";
@@ -11,7 +12,7 @@ const router = express.Router();
 router.post("/create", verifyTokens, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyTokens, likeComment);
-//router.put("/editComment/:commentId", verifyToken, editComment);
+router.put("/editComment/:commentId", verifyTokens, editComment);
 //router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
 //router.get("/getcomments", verifyToken, getcomments);
 
